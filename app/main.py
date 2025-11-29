2a2563ca1b95658ae026af5440d0d653f37908d9
Alonso-dfg <daniel.alonsog01@gmail.com>
2025-11-28 22:55:50 -0500

    creando el templates index.html

--- app/main.py
+++ app/main.py
@@ -1,22 +1,45 @@
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+
 from app.database import Base, engine
 from app.routers import productos, categorias, proveedores, usuario, externos
 
 # Crear tablas en la base de datos
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title = "API Productos - Proyecto Integrador")
+app = FastAPI(title="API Productos - Proyecto Integrador")
 
-# Incluir las rutas
+# Archivos estáticos
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Templates
+templates = Jinja2Templates(directory="app/templates")
+
+# Routers
 app.include_router(productos.router)
 app.include_router(categorias.router)
 app.include_router(proveedores.router)
 app.include_router(usuario.router)
 app.include_router(externos.router)
 
+
 app.mount("/static", StaticFiles(directory="app/static"), name="static")    
 
 @app.get("/")
 def inicio():
     return {"mensaje": "API funcionando "}
+
+# Página principal
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request}
+    )
+
