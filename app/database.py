57525a863a7a03ecabe1a1548db2392274b2d1c2
Alonso-dfg <daniel.alonsog01@gmail.com>
2025-12-02 20:49:21 -0500

    Todo funcional por el momento

--- app/database.py
+++ app/database.py
@@ -2,12 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./productos.db"
-
+SQLALCHEMY_DATABASE_URL = "postgresql://u1c2tiai7lxtthta8brd:7lVTMC3WLQpACUokG7AZK5UoO3nHDH@bhrdihak9xywof9dezre-postgresql.services.clever-cloud.com:50013/bhrdihak9xywof9dezre"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
